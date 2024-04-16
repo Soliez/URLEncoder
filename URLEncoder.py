620c4736c3dd2a39993f8b47a06508b88b01b7fb
Erik Solis <solie2@icloud.com>
2024-04-16 04:44:14 -0400

    Added buttons to clear and copy the text in the results box

--- URLEncoder.py
+++ URLEncoder.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import pyperclip
 from urllib.parse import quote, unquote
 
 
@@ -25,6 +26,24 @@ def decode_text() -> None:
     result_box.insert(tk.END, decoded_text)    
     result_box.config(state=tk.DISABLED)       
 
+
+def copy_to_clipboard() -> None:
+    '''
+    Copies the text from the textbox to the user's clipboard
+    '''
+    result_box_text = result_box.get("1.0", tk.END).strip()
+    pyperclip.copy(result_box_text)
+
+
+def clear_result() -> None:
+    '''
+    Clears the text in the texbox
+    '''
+    result_box.config(state=tk.NORMAL)
+    result_box.delete("1.0", tk.END)
+    result_box.config(state=tk.DISABLED)
+
+
 root = tk.Tk()
 
 root.geometry("500x500")
@@ -40,12 +59,21 @@ button_container = tk.Frame(root)
 button_container.pack(pady=10)
 
 encode_button = tk.Button(button_container, text="Encode", font=("Calibri",18), command=encode_text)
-encode_button.grid(row=0, column=0, padx=10)
+encode_button.pack(side=tk.LEFT, padx=10)
 
 decode_button = tk.Button(button_container, text="Decode", font=("Calibri", 18), command=decode_text)
-decode_button.grid(row=0, column=1, padx=10)
+decode_button.pack(side=tk.LEFT, padx=10)
 
 result_box = tk.Text(root, height=5, font=("Calibri", 16), state=tk.DISABLED)
 result_box.pack()
+
+cc_button_container = tk.Frame(root)
+cc_button_container.pack(pady=10)
+
+copy_button = tk.Button(cc_button_container, text="Copy", font=("Calibri", 18), command=copy_to_clipboard)
+copy_button.pack(side=tk.LEFT, padx=10)
+
+clear_button = tk.Button(cc_button_container, text="Clear", font=("Calibri", 18), command=clear_result)
+clear_button.pack(side=tk.LEFT, padx=10)
 
 root.mainloop()
